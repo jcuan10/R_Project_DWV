9acdab9943339c7427865500e3216b5e2d27e8c9
jcuan10 <97613555+jcuan10@users.noreply.github.com>
2022-02-13 12:32:32 -0600

    Add files via upload

--- R_Project_DWV/R_DWV_Proj.R
+++ R_Project_DWV/R_DWV_Proj.R
@@ -18,81 +18,182 @@ library(tidyverse)
 library(Hmisc)
 library(corrplot)
 library(dplyr)
+library(ggfortify)
 ###############################################################################
 
 #Loadind dataset and getting some basic information ###########################
 setwd("~/R_Project_DWV")
-DWV <- read.csv("daily-website-visitors.csv") #Dataset 
-str(DWV)
-head(DWV)
-tail(DWV)
+DWV <- read.csv("daily-website-visitors.csv")     # Loading Dataset 
+str(DWV)                                          # Checking structure of DWV
+head(DWV)                                         # Original "DWV" head
+#tail(DWV)
 ###############################################################################
 
-# Initial data preparation ####################################################
+# Initial data preparation and transformation #################################
+
+# 4 important variables need to be transformed as numeric variables:
+# "Page.Loads" "Unique.Visits" "First.Time.Visits" "Returning.Visits"
 
 DWV$Page.Loads <- as.numeric(gsub(",","",DWV$Page.Loads))
 DWV$Unique.Visits <- as.numeric(gsub(",","",DWV$Unique.Visits))
 DWV$First.Time.Visits <- as.numeric(gsub(",","",DWV$First.Time.Visits))
 DWV$Returning.Visits <- as.numeric(gsub(",","",DWV$Returning.Visits))
 
-Date <- as.Date(DWV$Date, format = "%m/%d/%Y")
-DWV$Date <- NULL
-DWV$Row <- NULL
+DWV <- DWV %>%
+           rename(
+                  A_Page.Loads = Page.Loads,
+                  B_Unique.Visits = Unique.Visits,
+                  C_First.Time.Visits = First.Time.Visits,
+                  D_Returning.Visits = Returning.Visits
+                 )
 
-DWV2 <- DWV
+Date <- as.Date(DWV$Date, format = "%m/%d/%Y") # Time series base (TS)
+DWV$Date <- NULL                               # Removing TS from DWV
+DWV$Row <- NULL                                # Removing some garbage
 
-DWV$Day <- NULL
+DWV2 <- DWV                                    # Saving DWV, still Dataframe 
 
-DWV_XTS <- as.xts(DWV, Date) # Convert dataframe DWV into time series DWV_XTS
+DWV$Day <- NULL                                # Removing "Day" cat. var.
 
-sum(is.na(DWV_XTS))
-summary(DWV_XTS)
-str(DWV_XTS)
-head(DWV_XTS)
+DWV_XTS <- as.xts(DWV, Date)        # Convert DWV into xts() object "DWV_XTS"
 
-DWV2_XTS <- DWV_XTS
-DWV2_XTS$Day.Of.Week <- NULL
+sum(is.na(DWV_XTS))                            # Checking for NA values
+summary(DWV_XTS)                               # Summary of our xTS object
+str(DWV_XTS)                                   # Verifying Structure of xTS
+head(DWV_XTS)                                  # Visualizing head of the xTS
+
+DWV2_XTS <- DWV_XTS                            # Saving DWV_XTS just in case
+DWV2_XTS$Day.Of.Week <- NULL                   # Removing unnecessary var.
 head(DWV2_XTS)
 
 ###############################################################################
 
 # Initial data visualization #####################################
-# Variables to be analized: Page.Loads, Unique.Visits, First.Time.Visits, Returning.Visits
+# Variables: Page.Loads, Unique.Visits, First.Time.Visits, Returning.Visits
 
-#hist.data.frame(DWV[,2:5])
+data_long <- DWV[,2:5] %>%  pivot_longer(colnames(DWV[,2:5])) %>% as.data.frame()
+ggplot(data_long, aes(x = value)) + geom_histogram(aes(y = ..density..)) + geom_histogram(color="black", fill="lightblue") + labs(title = "Histogram", x = "Values", y = "Density") + facet_wrap(~ name, scales = "free")
+
+DWV2$Day[DWV2$Day == "Sunday"] <- "Sun" 
+DWV2$Day[DWV2$Day == "Monday"] <- "Mon" 
+DWV2$Day[DWV2$Day == "Tuesday"] <- "Tue" 
+DWV2$Day[DWV2$Day == "Wednesday"] <- "Wed" 
+DWV2$Day[DWV2$Day == "Thursday"] <- "Thu" 
+DWV2$Day[DWV2$Day == "Friday"] <- "Fri" 
+DWV2$Day[DWV2$Day == "Saturday"] <- "Sat" 
+
 grp_bday <- group_by(DWV2, Day)
-SUM_bday <- summarise_all(grp_bday, funs(sum))
+SUM_bday <- summarise_all(grp_bday, funs(mean))
 SUM_bday$Day.Of.Week <- NULL
 require(tidyr)
 df.long <- gather(SUM_bday, variable, value, -Day)
-ggplot(data = df.long, aes(x = reorder(Day, -value), y = value, fill = variable)) + geom_col(position = position_dodge()) 
-
-
-data_long <- DWV[,2:5] %>%  pivot_longer(colnames(DWV[,2:5])) %>% as.data.frame()
-ggplot(data_long, aes(x = value)) + geom_histogram(aes(y = ..density..)) + geom_density(col = "red") + facet_wrap(~ name, scales = "free")
+ggplot(data = df.long, aes(x = reorder(Day, -value), y = value, fill = variable)) + geom_col(position = position_dodge()) + labs(title = "Average by Variable ", x = "Day of the week", y = "average")
 
 
 cor(DWV2_XTS) # Pearson Correlation
 cor(DWV2_XTS, method = "spearman")
 
-corrplot.mixed(cor(DWV2_XTS), lower = 'shade', upper = 'pie', order = 'hclust')
+#corrplot.mixed(cor(DWV2_XTS), lower = 'shade', upper = 'pie', order = 'hclust')
 corrplot.mixed(cor(DWV2_XTS), order = 'AOE')
+
+
+
 
 # Time Series analisys ########################################################
 
-# Calculate Periodicity, Cycle 
-#periodicity(DWV2_XTS)
-#cycle(DWV2_XTS)
-#par(mfrow = c(1, 1))
-#barplot(DWV2_XTS$Page.Loads)
-
-plot(as.zoo(DWV2_XTS), screens = 1, lty = 1:4, xlab = "Date", ylab = "Val", col = rainbow(4)) 
-legend("topright", c("Page.Loads", "Unique.Visits", "First.Time.Visits", "Returning.Visits"), lty = 1:5, cex = 0.5, col = rainbow(4))
+plot(as.zoo(DWV2_XTS), screens = 1, lty = 1:4,  main = "Page Loads & Visitors over time ", xlab = "Date", ylab = "value", col = rainbow(4)) 
+legend("topright", c("Page.Loads", "Unique.Visits", "First.Time.Visits", "Returning.Visits"), lty = 1:5, cex = 0.4, col = rainbow(4))
 
 
-#layout.matrix <- matrix(c(1, 2, 3, 4), nrow = 4, ncol = 1)
-par(mfrow = c(4, 1), mex = 0.5, cex = 0.5)
-plot(DWV2_XTS$Page.Loads, main = "Page Loads", col = '#FF0000')
-plot(DWV2_XTS$Unique.Visits, main = "Unique Visits", col = "#80FF00")
-plot(DWV2_XTS$First.Time.Visits, main = "First Time Visits", col = "#00FFFF")
-plot(DWV2_XTS$Returning.Visits, main = "Returning Visits", col = "#8000FF")
+# Original Time Series
+par(mfrow = c(4, 1), mex = 1, cex = 0.4)
+plot(DWV2_XTS$A_Page.Loads, main = "Page Loads", col = "#FF0000")
+plot(DWV2_XTS$B_Unique.Visits, main = "Unique Visits", col = "#80FF00")
+plot(DWV2_XTS$C_First.Time.Visits, main = "First Time Visits", col = "#00FFFF")
+plot(DWV2_XTS$D_Returning.Visits, main = "Returning Visits", col = "#8000FF")
+
+# Monthly Trend (Seasonal)
+par(mfrow = c(4, 1), mex = 1, cex = 0.4)
+month_tr = apply.monthly(DWV2_XTS, mean)
+barplot(month_tr$A_Page.Loads, main = "Page Loads / month", ylab = "Average" , col = "#FF0000")
+barplot(month_tr$B_Unique.Visits, main = "Unique Visits / month", ylab = "Average" , col = "#80FF00")
+barplot(month_tr$C_First.Time.Visits, main = "First Time Visits / month", ylab = "Average" , col = "#00FFFF")
+barplot(month_tr$D_Returning.Visits, main = "Returning Visits / month", ylab = "Average" , col = "#8000FF")
+
+# Quartely Trend (Trend)
+par(mfrow = c(4, 1), mex = 1, cex = 0.4)
+quar_tr = apply.quarterly(DWV2_XTS, mean)
+barplot(quar_tr$A_Page.Loads, main = "Page Loads / quarter", ylab = "Average" , col = "#FF0000")
+barplot(quar_tr$B_Unique.Visits, main = "Unique Visits / quarter", ylab = "Average" , col = "#80FF00")
+barplot(quar_tr$C_First.Time.Visits, main = "First Time Visits / quarter", ylab = "Average" , col = "#00FFFF")
+barplot(quar_tr$D_Returning.Visits, main = "Returning Visits / quarter", ylab = "Average" , col = "#8000FF")
+
+# Yearly Trend (Trend)
+par(mfrow = c(4, 1), mex = 1, cex = 0.4)
+year_tr = apply.yearly(DWV2_XTS, mean)
+barplot(year_tr$A_Page.Loads, main = "Page Loads / year", ylab = "Average" , col = "#FF0000")
+barplot(year_tr$B_Unique.Visits, main = "Unique Visits / year", ylab = "Average" , col = "#80FF00")
+barplot(year_tr$C_First.Time.Visits, main = "First Time Visits / year", ylab = "Average" , col = "#00FFFF")
+barplot(year_tr$D_Returning.Visits, main = "Returning Visits / year", ylab = "Average" , col = "#8000FF")
+
+
+# 
+# Analizing just the "Page Loads" and "Unique Visits"
+
+# Moving Average
+par(mfrow = c(2, 1), mex = 1, cex = 0.4)
+PL_MA <- rollmean(DWV2_XTS$A_Page.Loads, 7, align = "right")
+PL_MA <- merge(PL_MA, DWV2_XTS$A_Page.Loads)
+plot(PL_MA, main = "Moving Average - Page Loads")
+
+UV_MA <- rollmean(DWV2_XTS$B_Unique.Visits, 7, align = "right")
+UV_MA <- merge(UV_MA, DWV2_XTS$B_Unique.Visits)
+plot(UV_MA, main = "Moving Average - Unique Visits")
+
+# Autocorrelation
+acf(DWV2_XTS$A_Page.Loads, main = "Autocorrelation - Page Loads")
+
+acf(DWV2_XTS$B_Unique.Visits, main = "Autocorrelation - Unique Visits")
+
+# Autocorrelation Test with Box.test()
+AT_PL <- Box.test(DWV2_XTS$A_Page.Loads)
+AT_UV <- Box.test(DWV2_XTS$B_Unique.Visits)
+
+# Partial Autocorrelation
+pacf(DWV2_XTS$A_Page.Loads, main = "Partial Autocorrelation - Page Loads")
+
+pacf(DWV2_XTS$B_Unique.Visits, main = "Partial Autocorrelation - Unique Visits")
+
+#Lagged Correlation Between Page Loads and Unique Visits "Cross-Correlation Function
+par(mfrow = c(2, 1), mex = 0.5, cex = 0.5)
+plot(coredata(DWV2_XTS$A_Page.Loads)[,1], coredata(DWV2_XTS$B_Unique.Visits)[,1], main = "Page Loads vs Unique Visits", xlab =  "Page Loads", ylab = "Unique Visits")
+abline(reg = lm(coredata(DWV2_XTS$B_Unique.Visits)[,1] ~ coredata(DWV2_XTS$A_Page.Loads)[,1]), col = "red")
+Ccf(coredata(DWV2_XTS$A_Page.Loads)[,1], coredata(DWV2_XTS$B_Unique.Visits)[,1], main = "Page Loads vs Unique Visits")
+
+
+# Detrending the Time Series
+par(mfrow = c(2, 1), mex = 0.5, cex = 0.5)
+plot(PL_MA, main = "Page Loads Trend")
+m_PL <- lm(coredata(DWV2_XTS$A_Page.Loads) ~ index(DWV2_XTS))
+detr_PL <- zoo(resid(m_PL), index(DWV2_XTS))
+plot(detr_PL, xlab ="Year", ylab = "Residual from Linear Model", main = "Page Loads Deviation from Trend")
+
+par(mfrow = c(2, 1), mex = 0.5, cex = 0.5)
+plot(UV_MA, main = "Unique Visitd Trend")
+m_UV <- lm(coredata(DWV2_XTS$B_Unique.Visits) ~ index(DWV2_XTS))
+detr_UV <- zoo(resid(m_UV), index(DWV2_XTS))
+plot(detr_UV, xlab ="Year", ylab = "Residual from Linear Model", main = "Unique Visits Deviation from Trend")
+
+# ARIMA Model
+AAm_PL <- auto.arima(DWV2_XTS$A_Page.Loads)
+checkresiduals(AAm_PL)
+
+AAm_UV <- auto.arima(DWV2_XTS$B_Unique.Visits)
+checkresiduals(AAm_UV)
+
+# Forecast
+fc_PL <- forecast(AAm_PL, h=100)
+autoplot(fc_PL)
+
+fc_UV <- forecast(AAm_UV, h=100)
+autoplot(fc_UV)
